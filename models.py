54e71323145940ae933b7daaedfc86d2b45242ad
Marshallb03 <157312409+Marshallb03@users.noreply.github.com>
2024-06-11 16:16:58 -0400

    Add files via upload

---
+++ models.py
@@ -0,0 +1,38 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import  UserMixin, LoginManager 
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+
+class User(UserMixin, db.Model):
+    UserID = db.Column(db.Integer, primary_key=True)
+    Username = db.Column(db.String(255))
+    Password = db.Column(db.String(255))
+    role = db.Column(db.String(255))
+
+class College(db.Model):
+    UniversityID = db.Column(db.Integer, primary_key=True)
+    UniversityName = db.Column(db.String(255))
+    AcceptanceRate = db.Column(db.Integer)
+    AverageSAT = db.Column(db.Integer)
+    QSWorldRankings = db.Column(db.Integer)
+
+    USNewsRanking2023 = db.Column(db.Integer)
+    QSWorldRanking2022 = db.Column(db.Integer)
+    UndergraduatePopulation = db.Column(db.Integer)
+    AcademicGrade = db.Column(db.String(5))
+    AthleticGrade = db.Column(db.String(5))
+    CampusGrade = db.Column(db.String(5))
+    CampusFoodGrade = db.Column(db.String(5))
+    DiversityGrade = db.Column(db.String(5))
+    DormsGrade = db.Column(db.String(5))
+    LocationGrade = db.Column(db.String(5))
+    OverallGrade = db.Column(db.String(5))
+    PartySceneGrade = db.Column(db.String(5))
+    ProfessorsGrade = db.Column(db.String(5))
+    SafetyGrade = db.Column(db.String(5))
+    StudentLifeGrade = db.Column(db.String(5))
+    ValueGrade = db.Column(db.String(5))
+    InStateTuition = db.Column(db.Float)
+    OutOfStateTuition = db.Column(db.Float)
