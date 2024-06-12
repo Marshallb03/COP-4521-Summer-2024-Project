4e9e813a4ec65cadbcb376f0fa3ba08a437ab7a1
Marshallb03 <157312409+Marshallb03@users.noreply.github.com>
2024-06-11 21:22:15 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,15 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import  UserMixin, LoginManager 
-
 db = SQLAlchemy()
-login_manager = LoginManager()
 
 
 class User(UserMixin, db.Model):
     UserID = db.Column(db.Integer, primary_key=True)
     Username = db.Column(db.String(255))
     Password = db.Column(db.String(255))
-    role = db.Column(db.String(255))
+    role = db.Column(db.String(80), db.ForeignKey('role.name'))
+    
 
 class College(db.Model):
     UniversityID = db.Column(db.Integer, primary_key=True)
@@ -17,7 +16,6 @@ class College(db.Model):
     AcceptanceRate = db.Column(db.Integer)
     AverageSAT = db.Column(db.Integer)
     QSWorldRankings = db.Column(db.Integer)
-
     USNewsRanking2023 = db.Column(db.Integer)
     QSWorldRanking2022 = db.Column(db.Integer)
     UndergraduatePopulation = db.Column(db.Integer)
@@ -36,3 +34,8 @@ class College(db.Model):
     ValueGrade = db.Column(db.String(5))
     InStateTuition = db.Column(db.Float)
     OutOfStateTuition = db.Column(db.Float)
+
+class Role(db.Model, RoleMixin):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
