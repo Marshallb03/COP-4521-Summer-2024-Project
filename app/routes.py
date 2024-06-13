a1c00880a0606c3974cc4ef31410102d72b51207
Marshallb03 <157312409+Marshallb03@users.noreply.github.com>
2024-06-13 16:06:23 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,flash, redirect,url_for,session,logging,
 from flask_sqlalchemy import SQLAlchemy
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
