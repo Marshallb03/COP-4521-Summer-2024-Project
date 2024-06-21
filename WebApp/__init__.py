f39faf8ae383198fee903201908902b071b00227
Marshallb03 <157312409+Marshallb03@users.noreply.github.com>
2024-06-21 00:32:30 -0400

    Update __init__.py

--- WebApp/__init__.py
+++ WebApp/__init__.py
@@ -14,6 +14,7 @@ def create_app():
     db.init_app(app)
 
     login_manager = LoginManager()
+    login_manager.init_app(app)
     login_manager.login_view = 'login'
     login_manager.init_app(app)
 
